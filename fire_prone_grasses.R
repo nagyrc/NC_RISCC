a2788a3a8bf0e3364c072e39d28b856dc28201c3
nagyrc <nagyrc@gmail.com>
2021-07-30 14:05:47 -0600

    total fail

--- fire_prone_grasses.R
+++ fire_prone_grasses.R
@@ -4,6 +4,9 @@
 library(rgdal)
 library(sf)
 library(tidyverse)
+library(ggplot2)
+library(maptools)
+library(FRK)
 
 abbrev <- "CO" # set state of interest 
 
@@ -12,7 +15,7 @@ abbrev <- "CO" # set state of interest
 getwd()
 
 ## read in states shapefile (WGS 84)
-srg=readOGR("data/US_states/US_states.shp", layer="US_states")
+srg <- readOGR("data/US_states/US_states.shp", layer="US_states")
 proj4string(srg) #"+proj=longlat +datum=WGS84 +no_defs "
 head(srg@data)
 
@@ -81,7 +84,7 @@ dim(cty_select)
 length(unique(cty_select$CountyName))
 #now have county info by state
 
-#bring in grass info
+#bring in grass info for each species
 BRTE <- read.csv(file = 'data/BRTE_countytable.csv')
 IMCY <- read.csv(file = 'data/IMCY_countytable.csv')
 MISI <- read.csv(file = 'data/MISI_countytable.csv')
@@ -96,148 +99,121 @@ TACA <- read.csv(file = 'data/TACA8_countytable.csv')
 #county_shape@data <- merge(county_info, county_shape@data, by="GEOID", all.x=TRUE)
 cty@data <- merge(counties2, cty@data, by=c("GEOID", "STATEFP"), all.x=TRUE)
 
+#subset to NC
+NC <- cty[cty@data$State == "CO"|cty@data$State == "KS"|cty@data$State == "NE"|cty@data$State == "WY"|cty@data$State == "SD"|cty@data$State == "ND"|cty@data$State == "MT",]
+#460 observations
+
 #merge grass data
-head(cty@data)
+head(NC@data)
 head(BRTE)
 BRTE$GEOID <- as.factor(BRTE$GEOID)
 is.factor(BRTE$GEOID)
-cty@data <- merge(BRTE, cty@data, by="GEOID")
 
+#add cheatgrass
+NC@data <- merge(BRTE, NC@data, by="GEOID")
 
-#subset to NC region
-#NC <- cty@data %>%
-  #filter(State == "CO"|State == "KS"|State == "NE"|State == "WY"|State == "SD"|State == "ND"|State == "MT")
-
-NC <- cty[cty@data$State == "CO"|cty@data$State == "KS"|cty@data$State == "NE"|cty@data$State == "WY"|cty@data$State == "SD"|cty@data$State == "ND"|cty@data$State == "MT",]
 
 
 ggplot(NC) +
   geom_sf()
 
 
-plot(NC)
+#plot(NC)
 
 
-# in order to plot polygons, first fortify the data
-NC@data$id <- rownames(NC@data)
-#Fire-prone invasive grasses for the North Central US
-#R. Chelsea Nagy, Emily Fusco, Jenica Allen
-
-library(rgdal)
-library(sf)
-library(tidyverse)
-
-abbrev <- "CO" # set state of interest 
-
-### SUBSET MAP TO SELECTED STATE
-
-getwd()
-
-## read in states shapefile (WGS 84)
-srg=readOGR("data/US_states/US_states.shp", layer="US_states")
-proj4string(srg) #"+proj=longlat +datum=WGS84 +no_defs "
-head(srg@data)
-
-## Create a state list for reference
-states <- as.data.frame(cbind(as.character(srg@data$STUSPS), as.character(srg@data$STATEFP), as.character(srg@data$NAME)), stringsAsFactors = F)
-states
-
-head(states)
-str(states)
-colnames(states) <- c("Abbrev", "STATEFP", "State")
-
-## subset spatial data to state of interest
-chosen_state <- srg[grep(abbrev, srg$STUSPS),]
-chosen_statefp <- as.character(chosen_state@data$STATEFP)
-
-## check map for accurate subsetting
-plot(srg)
-plot(chosen_state, col = "red", add = T) #all code above works
+NCdf <- SpatialPolygonsDataFrame_to_df(NC, vars = names(NC))
 
 
-#### GET LIST OF GEOID IN CHOSEN STATE
-
-### read in county shapefile
-cty <- readOGR("data/US_counties//WGS84_clip.shp", layer="WGS84_clip")
-proj4string(cty) #"+proj=longlat +datum=WGS84 +no_defs "
-head(cty@data)
-#plot(cty) this works but takes forever
-
-counties <- as.data.frame(cbind(as.character(cty@data$GEOID), as.character(cty@data$NAMELSAD), as.character(cty@data$STATEFP)), stringsAsFactors = F)
-colnames(counties) <- c("GEOID", "CountyName", "STATEFP")
-head(counties)
-str(counties)
-dim(counties)
-
-
-## county table does not include state name, add state names for ease of use
-## create data object to catch output in loop
-counties1 <- cbind("99999", "Example County", "XX", "XX")
-colnames(counties1) <- c("GEOID", "CountyName", "STATEFP", "State")
-counties1
-
-
-for (i in unique(counties$STATEFP)){
-  
-  sub = counties[counties$STATEFP == i, ] #subsets counties of single state
-  sub1 = unique(states$Abbrev[states$STATEFP == i]) # returns single state abbreviation
-  
-  sub$State <- sub1 #adds state abbreviation to all counties in subset
-  
-  counties1 <- rbind(counties1, sub) #appends new data to county level dataset
-  
-  print(paste(sub1, "done", " "))
-  
+###
+if (require("maptools")) {
+  sids <- system.file("shapes/sids.shp", package="maptools")
+  nc1 <- readShapePoly(sids,
+                       proj4string = CRS("+proj=longlat +datum=NAD27"))
+  nc1_df <- fortify(nc1)
 }
 
-head(counties1)
-dim(counties1)
-
-counties2 <- counties1[2:3109, ] #remove junk row from object creation
-head(counties2)
-dim(counties2)
-
-cty_select <- counties2[counties2$State == abbrev, ] #get list of counties in chosen state
-head(cty_select)
-dim(cty_select)
-length(unique(cty_select$CountyName))
-#now have county info by state
-
-#bring in grass info
-BRTE <- read.csv(file = 'data/BRTE_countytable.csv')
-IMCY <- read.csv(file = 'data/IMCY_countytable.csv')
-MISI <- read.csv(file = 'data/MISI_countytable.csv')
-MIVI <- read.csv(file = 'data/MIVI_countytable.csv')
-NERE <- read.csv(file = 'data/NERE_countytable.csv')
-PECI <- read.csv(file = 'data/PECI_countytable.csv')
-SCBA <- read.csv(file = 'data/SCBA_countytable.csv')
-TACA <- read.csv(file = 'data/TACA8_countytable.csv')
 
 
-#add state names to cty
-#county_shape@data <- merge(county_info, county_shape@data, by="GEOID", all.x=TRUE)
-cty@data <- merge(counties2, cty@data, by=c("GEOID", "STATEFP"), all.x=TRUE)
-
-#merge grass data
-head(cty@data)
-head(BRTE)
-BRTE$GEOID <- as.factor(BRTE$GEOID)
-is.factor(BRTE$GEOID)
-cty@data <- merge(BRTE, cty@data, by="GEOID")
-
-
-#subset to NC region
-#NC <- cty@data %>%
-#filter(State == "CO"|State == "KS"|State == "NE"|State == "WY"|State == "SD"|State == "ND"|State == "MT")
-
-NC <- cty[cty@data$State == "CO"|cty@data$State == "KS"|cty@data$State == "NE"|cty@data$State == "WY"|cty@data$State == "SD"|cty@data$State == "ND"|cty@data$State == "MT",]
-
-
-ggplot(NC) +
-  geom_sf()
+###
+fortify.SpatialPolygonsDataFrame <- function(model, data, region = NULL, ...) {
+  attr <- as.data.frame(model)
+  # If not specified, split into regions based on polygons
+  if (is.null(region)) {
+    coords <- ldply(model@polygons,fortify)
+    message("Regions defined for each Polygons")
+  } else {
+    cp <- sp::polygons(model)
+    try_require("maptools")
+    # Union together all polygons that make up a region
+    unioned <- unionSpatialPolygons(cp, attr[, region])
+    coords <- fortify(unioned)
+    coords$order <- 1:nrow(coords)
+  }
+  coords
+}
 
 
-plot(NC)
+fortify.SpatialPolygons <- function(model, data, ...) {
+  ldply(model@polygons, fortify)
+}
+
+
+fortify.Polygons <- function(model, data, ...) {
+  subpolys <- model@Polygons
+  pieces <- ldply(seq_along(subpolys), function(i) {
+    df <- fortify(subpolys[[model@plotOrder[i]]])
+    df$piece <- i
+    df
+  })
+  within(pieces,{
+    order <- 1:nrow(pieces)
+    id <- model@ID
+    piece <- factor(piece)
+    group <- interaction(id, piece)
+  })
+}
+
+
+fortify.Polygon <- function(model, data, ...) {
+  df <- as.data.frame(model@coords)
+  names(df) <- c("long", "lat")
+  df$order <- 1:nrow(df)
+  df$hole <- model@hole
+  df
+}
+
+
+fortify.SpatialLinesDataFrame <- function(model, data, ...) {
+  ldply(model@lines, fortify)
+}
+
+
+fortify.Lines <- function(model, data, ...) {
+  lines <- model@Lines
+  pieces <- ldply(seq_along(lines), function(i) {
+    df <- fortify(lines[[i]])
+    df$piece <- i
+    df
+  })
+  within(pieces,{
+    order <- 1:nrow(pieces)
+    id <- model@ID
+    piece <- factor(piece)
+    group <- interaction(id, piece)
+  })
+}
+
+
+fortify.Line <- function(model, data, ...) {
+  df <- as.data.frame(model@coords)
+  names(df) <- c("long", "lat")
+  df$order <- 1:nrow(df)
+  df
+}
+
+###
+
+
 
 
 # in order to plot polygons, first fortify the data
@@ -246,7 +222,6 @@ NC@data$id <- rownames(NC@data)
 NCdata <- fortify(NC, region = "id")
 # merge the "fortified" data with the data from our spatial object
 NCdf <- merge(NCdata, NC@data, by = "id")
-
 
 
 dataProjected@data$id <- rownames(dataProjected@data)
