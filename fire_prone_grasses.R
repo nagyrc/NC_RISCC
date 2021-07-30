1daa21ceffee1a1a45a41a189cbca54c4d3d49dd
nagyrc <nagyrc@gmail.com>
2021-07-30 10:45:37 -0600

    create new R script

---
+++ fire_prone_grasses.R
@@ -0,0 +1,82 @@
+#Fire-prone invasive grasses for the North Central US
+#R. Chelsea Nagy, Emily Fusco, Jenica Allen
+
+library(rgdal)
+
+abbrev <- "CO" # set state of interest 
+
+### SUBSET MAP TO SELECTED STATE
+
+getwd()
+
+## read in states shapefile (WGS 84)
+srg=readOGR("data/US_states/US_states.shp", layer="US_states")
+proj4string(srg) #"+proj=longlat +datum=WGS84 +no_defs "
+head(srg@data)
+
+## Create a state list for reference
+states <- as.data.frame(cbind(as.character(srg@data$STUSPS), as.character(srg@data$STATEFP), as.character(srg@data$NAME)), stringsAsFactors = F)
+states
+
+head(states)
+str(states)
+colnames(states) <- c("Abbrev", "STATEFP", "State")
+
+## subset spatial data to state of interest
+chosen_state <- srg[grep(abbrev, srg$STUSPS),]
+chosen_statefp <- as.character(chosen_state@data$STATEFP)
+
+## check map for accurate subsetting
+plot(srg)
+plot(chosen_state, col = "red", add = T) #all code above works
+
+
+#### GET LIST OF GEOID IN CHOSEN STATE
+
+### read in county shapefile
+cty=readOGR("data/US_counties//WGS84_clip.shp", layer="WGS84_clip")
+proj4string(cty) #"+proj=longlat +datum=WGS84 +no_defs "
+head(cty@data)
+#plot(cty) this works but takes forever
+
+counties <- as.data.frame(cbind(as.character(cty@data$GEOID), as.character(cty@data$NAMELSAD), as.character(cty@data$STATEFP)), stringsAsFactors = F)
+colnames(counties) <- c("GEOID", "CountyName", "STATEFP")
+head(counties)
+str(counties)
+dim(counties)
+
+
+## county table does not include state name, add state names for ease of use
+## create data object to catch output in loop
+counties1 <- cbind("99999", "Example County", "XX", "XX")
+colnames(counties1) <- c("GEOID", "CountyName", "STATEFP", "State")
+counties1
+
+
+for (i in unique(counties$STATEFP)){
+  
+  sub = counties[counties$STATEFP == i, ] #subsets counties of single state
+  sub1 = unique(states$Abbrev[states$STATEFP == i]) # returns single state abbreviation
+  
+  sub$State <- sub1 #adds state abbreviation to all counties in subset
+  
+  counties1 <- rbind(counties1, sub) #appends new data to county level dataset
+  
+  print(paste(sub1, "done", " "))
+  
+}
+
+head(counties1)
+dim(counties1)
+
+counties2 <- counties1[2:3109, ] #remove junk row from object creation
+head(counties2)
+dim(counties2)
+
+cty_select <- counties2[counties2$State == abbrev, ] #get list of counties in chosen state
+head(cty_select)
+dim(cty_select)
+length(unique(cty_select$CountyName))
+#now have county info by state
+
+#bring in grass info
